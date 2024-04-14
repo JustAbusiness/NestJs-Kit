e0eb97d0d58f03086ab5c41a94cf826c9ec0d702
ICT-LAP-023\ngochuy <huysanti123456@gmail.com>
2024-04-15 04:27:21 +0700

    second commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/user')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('')
+  @Render('home')
+  handleHomePage() {
+    const message = this.appService.getHello();
+
+    return {
+      message: message,
+    };
+    // return 'this.appService.getHello()';
   }
 
   getHello1(): string {
