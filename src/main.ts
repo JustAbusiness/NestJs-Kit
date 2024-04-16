8fc8b0c6dbab8eb9b283e5f075fffd58ba6725e6
ICT-LAP-023\ngochuy <huysanti123456@gmail.com>
2024-04-17 01:11:04 +0700

    setup mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
+import * as dotenv from 'dotenv';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  const configService = app.get(ConfigService);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3100);
+  await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
