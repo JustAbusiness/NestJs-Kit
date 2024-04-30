1dc4c1414bfe6892b3da0c4b6d9d544f4aae1acf
ICT-LAP-023\ngochuy <huysanti123456@gmail.com>
2024-04-30 19:49:46 +0700

    write access token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,20 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
+    private authService: AuthService,
   ) {}
 
-  @Get('')
-  @Render('home')
-  handleHomePage() {
-    console.log('check port', this.configService.get<string>('PORT'));
-    const message = this.appService.getHello();
-
-    return {
-      message: message,
-    };
-  }
-
-  getHello1(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  handleLogin(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
