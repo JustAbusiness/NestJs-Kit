d454ca1d98bd5bcbb76912ddbe7d94e51c6fc95c
ICT-LAP-023\ngochuy <huysanti123456@gmail.com>
2024-05-01 13:30:32 +0700

    write update and delete method

--- src/decorator/customize.ts
+++ src/decorator/customize.ts
@@ -7,6 +7,7 @@ import {
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true); //key: value
 
+// Passing Req.user
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
